dbf5cd633f562637ba56b1ae9c269ef905852b5b
Valentin Ochs <a@0au.de>
2012-08-31 17:38:40 +0200

    Removed unneeded functions from markov.h

--- markov.h
+++ markov.h
@@ -17,19 +17,13 @@ void
 markov_add(markov_t *m, wordlist_t *w);
 void
 markov_dump(void *strings, markov_t *m, int fd);
-markov_t *
-markov_find(markov_t *m, char *key);
-wordlist_t
+char *
 markov_getline(markov_t *m);
-markov_t *
-markov_insert(markov_t *m, markov_t *new);
 void
 markov_load(void **strings, markov_t **m, int fd);
 char *
 markov_next(markov_t *m, wordlist_t *w);
 wordlist_t *
 markov_randomstart(markov_t *m, wordlist_t *start);
-markov_t *
-markov_search(markov_t *m, char *key);
 
 #endif
